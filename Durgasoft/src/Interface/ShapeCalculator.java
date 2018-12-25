672d659be1b703e3e1fd2275b0e19294aa4fb6f6
ProgrammerJavaSelenium <ProgrammerJavaSelenium@gmail.com>
2018-12-25 21:40:29 +0530

    ShapeCalculator

---
+++ Durgasoft/src/Interface/ShapeCalculator.java
@@ -0,0 +1,62 @@
+package Interface;
+
+interface ShapeCalculator 
+{
+	void calculateArea();
+}
+
+class Rectangleshape implements ShapeCalculator
+{
+	public void calculateArea()
+	{
+		System.out.println("Rectangle Area");
+	}
+}
+class Circleshape implements ShapeCalculator
+{
+	public void calculateArea()
+	{
+		System.out.println("Circle Area");
+	}
+}
+
+class PlaywithCalculator
+{
+	ShapeCalculator SC;
+	
+	PlaywithCalculator(ShapeCalculator SC)
+	{
+		this.SC=SC;
+		SC.calculateArea();
+	}
+}
+
+class MainClass1
+{
+	public static void main(String[] args)
+	{
+		Rectangleshape Rec=new Rectangleshape();
+		Circleshape Cir=new Circleshape();
+		
+		PlaywithCalculator Pwc1=new PlaywithCalculator(Rec);
+		PlaywithCalculator Pwc2=new PlaywithCalculator(Cir);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
